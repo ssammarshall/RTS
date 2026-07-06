87d2cce1164ba6e2e07038eab3947f0a6ca75736
ssammarshall <ssammarshall@gmail.com>
2026-07-06 15:28:28 -0700

    Refactor: change command to unit command

--- commands/command.gd
+++
@@ -1,5 +0,0 @@
-class_name Command extends Node
-
-# Called upon to perform specific action.
-func execute(_unit: Unit, _delta: float) -> void:
-	pass
---
+++ commands/unit_command.gd
@@ -0,0 +1,13 @@
+class_name UnitCommand extends Resource
+
+# Called once UnitCommand is set to active command.
+func enter(_unit: Unit) -> void:
+	pass
+
+# Called upon to perform specific action.
+func execute(_unit: Unit, _delta: float) -> void:
+	pass
+
+# Called once UnitCommand is finished or changed.
+func exit(_unit: Unit) -> void:
+	pass
