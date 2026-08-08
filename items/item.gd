1ef95788aa2bb4451e37190f1b243b5150c0f0c8
ssammarshall <ssammarshall@gmail.com>
2026-08-08 10:22:49 -0700

    Add: get item type helper function

--- items/item.gd
+++ items/item.gd
@@ -4,6 +4,9 @@ class_name Item extends Node3D
 var data: ItemData
 var durability: int
 
+func get_item_type() -> ItemData.Type:
+	return data.type if data else ItemData.Type.NONE
+
 func use() -> bool:
 	if durability <= 0: return false
 	durability -= 1
